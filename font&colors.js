cdfdf4a7539369265ed08fb645dc7db184df643a
Rafi Siddique <rafisiddique67@gmail.com>
2024-09-25 05:19:13 +0600

    nav bar completed

---
+++ font&colors.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'button-primary': '#B4F461',
+                'text-primary': '#111111',
+                'nav-end-text': 'rgba(17, 17, 17, 0.7)',
+                'nav-bg-primary': 'rgb(249, 247, 243)',
+                'text-primary': 'rgb(17, 17, 17)',
+            },
+            fontFamily: {
+                'lexend': ['Lexend', 'sans-serif']
+            }
+        }
+    }
+}
