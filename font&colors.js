4a63b598a8d8663437c52cdb231501ede2e1ac94
Rafi Siddique <rafisiddique67@gmail.com>
2024-09-26 08:21:30 +0600

    history section done

--- font&colors.js
+++ font&colors.js
@@ -6,6 +6,7 @@ tailwind.config = {
                 'text-primary': '#111111',
                 'nav-end-text': 'rgba(17, 17, 17, 0.7)',
                 'nav-bg-primary': 'rgb(249, 247, 243)',
+                'nav-bg': 'rgb(249, 247, 243, 0.7)',
                 'text-primary': 'rgb(17, 17, 17)',
                 'btn-secondary': 'rgba(17, 17, 17, 0.05)',
             },
