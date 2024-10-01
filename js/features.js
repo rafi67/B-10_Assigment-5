ccefe770bcd97a8c01f66c18348636bc0fd06ce3
Rafi Siddique <rafisiddique67@gmail.com>
2024-10-01 23:57:14 +0600

    nav bar responsive done

--- js/features.js
+++ js/features.js
@@ -3,15 +3,26 @@ document.getElementById('history-btn').addEventListener('click', function() {
     removeClassFromDiv('footer', 'mt-20');
     addClassToDiv('footer', 'mt-72');
     selectButton('donation-btn', 'history-btn', false);
-}
-);
+});
 
+
+document.getElementById('history-anchor').addEventListener('click', function() {
+    showSectionGetById('history-section');
+    removeClassFromDiv('footer', 'mt-20');
+    addClassToDiv('footer', 'mt-72');
+});
 
 document.getElementById('donation-btn').addEventListener('click', function() {
     showSectionGetById('donation-section');
     removeClassFromDiv('footer', 'mt-72');
     addClassToDiv('footer', 'mt-20');
     selectButton('donation-btn', 'history-btn', true);
+});
+
+document.getElementById('donation-anchor').addEventListener('click', function() {
+    showSectionGetById('donation-section');
+    removeClassFromDiv('footer', 'mt-72');
+    addClassToDiv('footer', 'mt-20');
 });
 
 function addHistory(id, amount) {
